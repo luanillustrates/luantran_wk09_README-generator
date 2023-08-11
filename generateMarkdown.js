0098e49ae787a63531102cc91c82c91761f609c0
luanillustrates <luanillustrates@gmail.com>
2023-08-11 23:19:25 +0930

    completed more to-dos

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,19 +1,46 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]'
+    case 'Mozilla public license 2.0':
+      return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)]'
+    case 'The Unlicense':
+      return '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]'
+    default:
+      return '';
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return '<a href="https://opensource.org/licenses/MIT" target="_blank">here</a>'
+    case 'Mozilla public license 2.0':
+      return '<a href="https://opensource.org/licenses/MPL-2.0" target="_blank">here</a>'
+    case 'The Unlicense':
+      return '<a href="http://unlicense.org/" target="_blank">here</a>'
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
+function generateMarkdown(answers) {
+  return `
+  ${renderLicenseBadge(answers.license)}
+  # ${answers.title}
+  ## Project description
+  ${answers.description}
+  ## Table of Contents
+  ## License
+  ${answers.license}
+  License details can be found ${renderLicenseLink(answers.license)}
 `;
 }
 
--- index.js
+++ index.js
@@ -1,13 +1,70 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const { writeFile } = require('fs').promises;
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// array of questions for user input
+const questionPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'enter the project title',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'enter the project description',
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'describe the installation steps',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'describe the usage of the project',
+        },
+        {
+            type: 'input',
+            name: 'contribute',
+            message: 'list any contribution methods here',
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'enter test instructions',
+        },
+        {
+            type: 'input',
+            name: 'license',
+            choices: ['MIT', 'Mozilla public license 2.0', 'The Unlicense'],
+            message: 'select a license',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'enter your Github username',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'enter your email address',
+        },
+    ])
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {
+//     fs.writeFile(fileName, data)
+// }
 
-// Function call to initialize app
+const init = () => {
+    promptUser()
+        .then((answers) => writeFile('README.md', generateMarkdown(answers)))
+        .then(() => console.log('successfully wrote to README.md'))
+        .catch((err) => console.error(err));
+};
+
 init();
